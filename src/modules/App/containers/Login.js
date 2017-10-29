f1b935170b87a7a60a018d873008a39b06ee9331
cao shujian <cao.shujian@puscene.com>
2017-10-29 18:22:54 +0800

    add feature register

--- src/modules/App/containers/Login.js
+++ src/modules/App/containers/Login.js
@@ -5,36 +5,26 @@ import * as Path from 'constants/storePath';
 // import style from 'styles/login.scss';
 import { withRouter } from "react-router-dom";
 import { JSEncrypt } from "jsencrypt";
-import { List, InputItem, NavBar, Button, WhiteSpace } from "antd-mobile";
+import { List, InputItem, NavBar, Button, WhiteSpace, Toast } from "antd-mobile";
 import { createForm } from 'rc-form';
-// import { getPublicKey, login} from '../actions/profile';
+import { login, getPublicKey } from '../actions/profile';
 
 class Login extends Component {
   constructor (props) {
     super(props);
-    this.handleSubmit = this.handleSubmit.bind(this);
     this.goBack = this.goBack.bind(this);
+    this.onSubmitClick = this.onSubmitClick.bind(this);
+    this.validateForm = this.validateForm.bind(this);
+    this.encrypt = this.encrypt.bind(this);
     this.onRegisterClick = this.onRegisterClick.bind(this);
   }
 
-  async handleSubmit (e) {
-    e.preventDefault();
-    let fromData, error;
-    this.props.form.validateFields((err, values) => {
-      error = err;
-      fromData = values;
-    });
-    if (error) return;
-
-    const { publicKey } = this.props;
-    const passWord = this.encrypt(fromData.passWord);
-    const formData = { ...fromData, passWord, publicKey };
-    await this.props.login(formData);
-    // window.location.reload();
+  async componentDidMount () {
+    await this.props.getPublicKey();
   }
 
   encrypt (password) {
-    const encrypt = new JSEncrypt(); // 实例化加密对象
+    let encrypt = new JSEncrypt(); // 实例化加密对象
     encrypt.setPublicKey(this.props.publicKey); // 设置公钥
     return encrypt.encrypt(password);// 加密明文
   }
@@ -43,12 +33,42 @@ class Login extends Component {
     this.props.history.goBack();
   }
 
+  onSubmitClick () {
+    if (!this.validateForm())
+      return;
+    let { userName, passWord } = this.props.form.getFieldsValue();
+
+    const { publicKey } = this.props;
+    passWord = this.encrypt(passWord);
+    this.props.login({ userName, passWord, publicKey });
+    // todo get token and save it
+    // window.location.reload();
+    // localStorage.setItem('name', '123');
+    // console.log(localStorage.getItem('name'));
+  }
+
   onRegisterClick () {
     this.props.history.push('/register');
   }
 
+  validateForm () {
+    let error;
+    this.props.form.validateFields((err) => {
+      error = err;
+    });
+
+    if (error) {
+      for (let key in error) {
+        Toast.fail(this.props.form.getFieldError(key), 2);
+        return false;
+      }
+    }
+
+    return true;
+  }
+
   render () {
-    const { getFieldProps } = this.props.form;
+    const { getFieldProps, getFieldError } = this.props.form;
     return (
       <div>
         <NavBar
@@ -57,15 +77,26 @@ class Login extends Component {
           onLeftClick={this.goBack}>登录</NavBar>
         <List>
           <InputItem
-            {...getFieldProps('userName')}
+            {...getFieldProps('userName', {
+              rules: [
+                { required: true, message: '用户名不能为空' }
+              ]
+            })}
             clear
+            error={!!getFieldError('userName')}
             placeholder="用户名、邮箱或手机号"></InputItem>
           <InputItem
-            {...getFieldProps('password')}
+            {...getFieldProps('passWord', {
+              rules: [
+                { required: true, message: '密码不能为空' }
+              ]
+            })}
             type="password"
-            placeholder="密码"></InputItem>
+            error={!!getFieldError('passWord')}
+            placeholder="输入密码"></InputItem>
           <List.Item>
-            <Button type="primary" style={{ color: 'white' }}>登录</Button><WhiteSpace />
+            <Button type="primary" style={{ color: 'white' }} onClick={this.onSubmitClick}>登录</Button>
+            <WhiteSpace />
             <div style={{ width: '100%', color: '#108ee9', textAlign: 'center' }} onClick={this.onRegisterClick}>
               没有账号？马上注册一个
             </div>
@@ -85,7 +116,8 @@ function mapStateToProps (state) {
 
 function mapDispatchToProps (dispatch) {
   return {
-    // login: (payload) => dispatch(login(payload))
+    login: (payload) => dispatch(login(payload)),
+    getPublicKey: (payload) => dispatch(getPublicKey(payload))
   };
 }
 
